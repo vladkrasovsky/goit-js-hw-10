2a3305582af84dfb35110f2c256a515ff798d463
Vladyslav Krasovsky <v.o.krasovsky@gmail.com>
2022-10-06 11:34:38 +0300

    Adds references

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { refs } from './js/refs';
 import { fetchCountries } from './js/fetchCountries';
 import './css/styles.css';
 
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export const refs = {
+  searchBox: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
