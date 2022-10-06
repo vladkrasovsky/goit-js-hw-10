a57fc2d4c499ddd9fcd321168011721d1537ee47
Vladyslav Krasovsky <v.o.krasovsky@gmail.com>
2022-10-06 12:10:35 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,3 +1,17 @@
+const API_URL = 'https://restcountries.com/v3.1';
+
+const searchParams = new URLSearchParams({
+  fields: ['name', 'capital', 'population', 'flags', 'languages'],
+});
+
 export const fetchCountries = name => {
-  console.log('fetchCountries');
+  return fetch(`${API_URL}/name/${name}?${searchParams}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+  // .catch(error => {
+  //   console.error(error.message);
+  // });
 };
