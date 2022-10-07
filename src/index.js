70fd599302d055573efeb8e3b7fa0d05dafc3bda
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-10-07 22:46:42 +0300

    Adds task functionality

--- src/index.js
+++ src/index.js
@@ -5,3 +5,88 @@ import { fetchCountries } from './js/fetchCountries';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const MAX_COUNTRIES_QTY = 10;
+
+const onSearch = ({ target }) => {
+  const searchQuery = target.value.trim();
+
+  resetMarkup();
+  target.value = searchQuery;
+  searchQuery && fetchCountries(searchQuery).then(onSuccess).catch(onFailure);
+};
+
+const onClick = ({ target }) => {
+  if (target.nodeName !== 'UL') {
+    const searchQuery = target.dataset.name;
+
+    resetMarkup();
+    refs.searchBox.value = searchQuery;
+    fetchCountries(searchQuery, true).then(onSuccess).catch(onFailure);
+  }
+};
+
+refs.searchBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+refs.countryList.addEventListener('click', onClick);
+
+function onSuccess(data) {
+  const countriesQty = data.length;
+
+  if (countriesQty > MAX_COUNTRIES_QTY) {
+    return Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  }
+
+  if (countriesQty > 1) {
+    return renderCountryList(data);
+  }
+
+  renderCountryInfo(data[0]);
+}
+
+function onFailure() {
+  Notify.failure('Oops, there is no country with that name');
+}
+
+function resetMarkup() {
+  refs.countryInfo.innerHTML = '';
+  refs.countryList.innerHTML = '';
+}
+
+function renderCountryList(countries) {
+  if (!countries) return;
+
+  const markup = countries.reduce((acc, { name, flags }) => {
+    return (
+      acc +
+      `<li class="list-group-item list-group-item-action" data-name="${name.common}">
+        <img src="${flags.svg}" width="30">${name.common}
+      </li>`
+    );
+  }, '');
+
+  refs.countryList.innerHTML = markup;
+}
+
+function renderCountryInfo(country) {
+  if (!country) return;
+
+  const { name, capital, flags, population, languages } = country;
+  const langs = Object.values(languages).join(', ');
+
+  const markup = `
+    <div class="card">
+      <img src="${flags.svg}" class="card-img-top" alt="${name.common}">
+      <div class="card-body">
+        <h4 class="card-title mb-0">${name.common}</h4>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item"><b>Capital:</b> ${capital}</li>
+        <li class="list-group-item"><b>Population:</b> ${population}</li>
+        <li class="list-group-item"><b>Languages:</b> ${langs}</li>
+      </ul>
+    </div>
+  `;
+
+  refs.countryInfo.innerHTML = markup;
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,17 +1,15 @@
 const API_URL = 'https://restcountries.com/v3.1';
 
-const searchParams = new URLSearchParams({
-  fields: ['name', 'capital', 'population', 'flags', 'languages'],
-});
+export const fetchCountries = (name, fullText = false) => {
+  const searchParams = new URLSearchParams({
+    fields: ['name', 'capital', 'population', 'flags', 'languages'],
+    fullText,
+  });
 
-export const fetchCountries = name => {
   return fetch(`${API_URL}/name/${name}?${searchParams}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
     return response.json();
   });
-  // .catch(error => {
-  //   console.error(error.message);
-  // });
 };
